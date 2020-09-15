88dbd819a859b0ab7104094aa337ddc00c964ce7
Martin Diehl <m.diehl@mpie.de>
2020-09-15 16:20:10 +0200

    Fix Python installation

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,37 +5,22 @@ export FFLAGS="${FFLAGS} -lhdf5_fortran -lhdf5 -lfftw3_mpi -lfftw3"
 bash DAMASK_prerequisites.sh
 cat system_report.txt
 
-# Python Installation 
-cp -r python/damask ${STDLIB_DIR}
-cp VERSION ${PREFIX}/lib/VERSION
-for f in $(ls processing/misc/*); do
-    n=$(basename $f)
-    cp -r $f ${PREFIX}/bin/${n%.*};
-done
-for f in $(ls processing/pre/*); do
-    n=$(basename $f)
-    cp -r $f ${PREFIX}/bin/${n%.*};
-done
-for f in $(ls processing/post/*); do
-    n=$(basename $f)
-    cp -r $f ${PREFIX}/bin/${n%.*};
-done
-
-# Build Damask
+# Build grid solver
 mkdir build_grid
 cd build_grid 
-cmake -DDAMASK_SOLVER="grid" -DCMAKE_INSTALL_PREFIX="${PREFIX}/bin" ..
+cmake -DDAMASK_SOLVER="grid" -DCMAKE_INSTALL_PREFIX="${PREFIX}" ..
 make -j$CPU_COUNT install
-cp src/DAMASK_grid ${PREFIX}/bin
 cd ..
 
-# mkdir build_mesh
-# cd build_mesh
-# cmake -DDAMASK_SOLVER="mesh" -DCMAKE_INSTALL_PREFIX="${PREFIX}/bin" ..
-# make -j$CPU_COUNT install
-# cp src/DAMASK_mesh ${PREFIX}/bin
-# cd ..
+# Build mesh solver
+mkdir build_mesh
+cd build_mesh
+cmake -DDAMASK_SOLVER="mesh" -DCMAKE_INSTALL_PREFIX="${PREFIX}" ..
+make -j$CPU_COUNT install
+cd ..
 
+# Python Installation 
 cd python
 cp ../VERSION .
-pip install .
+python -m pip install . --no-deps -vv
+cp VERSION ${STDLIB_DIR}/site-packages/damask/VERSION
