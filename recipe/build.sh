63897fa828f453032f8e27efa1f1aa1a46e2e0eb
Jan Janssen <jan-janssen@users.noreply.github.com>
2021-07-14 23:05:31 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,7 @@
 export PETSC_DIR=${PREFIX}
-export FFLAGS="${FFLAGS} -lhdf5_fortran -lhdf5 -lfftw3f -lfftw3f_mpi -lfftw3l -lfftw3l_mpi"
+export FFLAGS="${FFLAGS} -lhdf5_fortran -lhdf5 -lfftw3 -lfftw3_mpi"
+export LD="mpif90"
+export LDFLAGS="${LDFLAGS} -fopenmp"
 
 # System report 
 bash DAMASK_prerequisites.sh
