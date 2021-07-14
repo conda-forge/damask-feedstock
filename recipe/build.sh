dc4c3d5adfa95f9b7d2a1f9c5d3f28cc8aa6fa08
Jan Janssen <jan-janssen@users.noreply.github.com>
2021-07-14 23:21:07 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,13 @@
 export PETSC_DIR=${PREFIX}
 export FFLAGS="${FFLAGS} -lhdf5_fortran -lhdf5 -lfftw3_mpi -lfftw3"
-export LD="mpif90"
-export LDFLAGS="${LDFLAGS} -fopenmp"
 
 # System report 
 bash DAMASK_prerequisites.sh
 cat system_report.txt
+
+# Overwrite Linker
+export LD="mpif90"
+export LDFLAGS="${LDFLAGS} -fopenmp"
 
 # Build grid solver
 mkdir build_grid
