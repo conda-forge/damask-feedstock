9e384a99f4dc4922e284bf7fd19d3917be4590b5
Jan Janssen <jan-janssen@users.noreply.github.com>
2021-07-15 06:29:45 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,6 @@
 export PETSC_DIR=${PREFIX}
 export FFLAGS="${FFLAGS} -lhdf5_fortran -lhdf5 -lfftw3_mpi -lfftw3"
+export LDFLAGS="${LDFLAGS} -lhdf5_fortran -lhdf5 -lfftw3_mpi -lfftw3"
 
 # System report 
 bash DAMASK_prerequisites.sh
@@ -8,14 +9,14 @@ cat system_report.txt
 # Build grid solver
 mkdir build_grid
 cd build_grid 
-cmake -DDAMASK_SOLVER="grid" -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_LINKER="${PREFIX}/bin/mpif90" -DCMAKE_CXX_LINK_EXECUTABLE="<CMAKE_LINKER> <FLAGS> <CMAKE_CXX_LINK_FLAGS> <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>" ..
+cmake -DDAMASK_SOLVER="grid" -DCMAKE_INSTALL_PREFIX="${PREFIX}" ..
 make -j$CPU_COUNT install
 cd ..
 
 # Build mesh solver
 mkdir build_mesh
 cd build_mesh
-cmake -DDAMASK_SOLVER="mesh" -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_LINKER="${PREFIX}/bin/mpif90" -DCMAKE_CXX_LINK_EXECUTABLE="<CMAKE_LINKER> <FLAGS> <CMAKE_CXX_LINK_FLAGS> <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>" ..
+cmake -DDAMASK_SOLVER="mesh" -DCMAKE_INSTALL_PREFIX="${PREFIX}" ..
 make -j$CPU_COUNT install
 cd ..
 
