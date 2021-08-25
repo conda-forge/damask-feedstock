4a0e85676d26b43de2742a03a4975ed68b39f120
Martin Diehl <mail@martin-diehl.net>
2021-08-25 15:22:36 +0200

    general polishing
    - minimizing dependencies
    - simplifying build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,20 +1,15 @@
-export PETSC_DIR=${PREFIX}
-
 # System report 
-bash DAMASK_prerequisites.sh
+PETSC_DIR=${PREFIX} bash DAMASK_prerequisites.sh
 cat system_report.txt
 
-# Build grid solver
-cmake -B build-grid -DDAMASK_SOLVER=grid -DCMAKE_INSTALL_PREFIX=${PREFIX}
+# Grid solver
+PETSC_DIR=${PREFIX} cmake -B build-grid -DDAMASK_SOLVER=grid -DCMAKE_INSTALL_PREFIX=${PREFIX}
 cmake --build build-grid --target install
 
-# Build mesh solver
-cmake -B build-mesh -DDAMASK_SOLVER=mesh -DCMAKE_INSTALL_PREFIX=${PREFIX}
+# Mesh solver
+PETSC_DIR=${PREFIX} cmake -B build-mesh -DDAMASK_SOLVER=mesh -DCMAKE_INSTALL_PREFIX=${PREFIX}
 cmake --build build-mesh --target install
 
-# Python Installation 
+# Pre- and post-processing tools
 cd python
-cp ../VERSION .
-python -m pip install . --no-deps -vv
-cp VERSION ${STDLIB_DIR}/site-packages/damask/VERSION
-cd ..
+$PYTHON setup.py install
