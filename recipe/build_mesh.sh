76b4e4bcf07ec51b3dd1bce601a1396dfeaec170
Isuru Fernando <isuruf@gmail.com>
2023-07-19 23:14:25 -0500

    Update build_mesh.sh for openmpi cross

--- recipe/build_mesh.sh
+++ recipe/build_mesh.sh
@@ -1,3 +1,4 @@
 if [ $CONDA_BUILD_CROSS_COMPILATION = '1' ]; then sed -i '' -e 's/ -mtune=native//g' cmake/Compiler-GNU.cmake; fi
+export OPAL_PREFIX=$PREFIX
 PETSC_DIR=${PREFIX} cmake -B build-mesh -DDAMASK_SOLVER=mesh -DCMAKE_INSTALL_PREFIX=${PREFIX}
 cmake --build build-mesh --target install
