992721040c4ac13a20967e740cb5ed87bb6aba58
Martin Diehl <mail@martin-diehl.net>
2024-01-24 16:30:24 +0100

    run example as test

--- recipe/build_grid.sh
+++ recipe/build_grid.sh
@@ -1,4 +1,4 @@
-if [ $CONDA_BUILD_CROSS_COMPILATION = '1' ]; then sed -i '' -e 's/ -mtune=native//g' cmake/Compiler-GNU.cmake; fi
+if [[ $CONDA_BUILD_CROSS_COMPILATION = '1' ]]; then sed -i '' -e 's/ -mtune=native//g' cmake/Compiler-GNU.cmake; fi
 export OPAL_PREFIX=${PREFIX}
 PETSC_DIR=${PREFIX} cmake -B build-grid -DDAMASK_SOLVER=grid -DCMAKE_INSTALL_PREFIX=${PREFIX} ${CMAKE_ARGS}
 cmake --build build-grid --target install
--- recipe/build_mesh.sh
+++ recipe/build_mesh.sh
@@ -1,4 +1,4 @@
-if [ $CONDA_BUILD_CROSS_COMPILATION = '1' ]; then sed -i '' -e 's/ -mtune=native//g' cmake/Compiler-GNU.cmake; fi
+if [[ $CONDA_BUILD_CROSS_COMPILATION = '1' ]]; then sed -i '' -e 's/ -mtune=native//g' cmake/Compiler-GNU.cmake; fi
 export OPAL_PREFIX=$PREFIX
 PETSC_DIR=${PREFIX} cmake -B build-mesh -DDAMASK_SOLVER=mesh -DCMAKE_INSTALL_PREFIX=${PREFIX} ${CMAKE_ARGS}
 cmake --build build-mesh --target install
